b41aada13c91232856fe423b7d78af1fcb9f8e5a
Leandro <contato@leandroaraujo.me>
2019-12-02 20:37:23 -0300

    sync

--- .history/packages/core/src/index_20191202132002.js
+++
@@ -1,9 +0,0 @@
-export { normalize } from './common';
-
-export { default as Accordion } from './components/Accordion/Accordion';
-export { default as Breadcrumb } from './components/Breadcrumb/Breadcrumb';
-export { default as Button } from './components/Button/Button';
-export { default as ButtonGroup } from './components/ButtonGroup/ButtonGroup';
-export { default as ButtonToolbar } from './components/ButtonToolbar/ButtonToolbar';
-export { default as Message } from './components/Message/Message';
-export { default as Provider } from './components/Provider/Provider';
